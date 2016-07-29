11a4328e9263d45b21c69c5d2e7be9d29ff11f14
Jasper Schulz <jasper.b.schulz@gmail.com>
2016-07-29 14:13:47 +0200

    Added tests for break/continue

--- tests/00_basic/02_control_flow.rs
+++ tests/00_basic/02_control_flow.rs
@@ -19,8 +19,33 @@ fn test_loop() {
     __assert(i == 10);
 }
 
+fn test_continue() {
+    let mut i = 0;
+    let mut sum = 0;
+    while i < 100 {
+        if i % 2 == 1 {
+            continue;
+        }
+        sum += i;
+    }
+
+    __assert(sum == 2450)
+}
+
+fn test_break() {
+    let mut i = 0;
+    while i < 10 {
+        if i == 6 {
+            break;
+        }
+        i += 1;
+    }
+    __assert(i == 5);
+}
+
 
 fn main() {
     test_if();
     test_loop();
+    test_continue();
 }
